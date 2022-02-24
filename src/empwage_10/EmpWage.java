d2c35e1eecc7ed55186b5d178af206f7f43f34d2
Rachappa-Minajagi <m.rachoti142@gmail.com>
2022-02-24 15:12:22 +0530

    Added Code :SwitchCase Statement

--- src/empwage_10/EmpWage.java
+++ src/empwage_10/EmpWage.java
@@ -2,26 +2,35 @@ package empwage_10;
 import java.util.Random; // import Random class
 
 public class EmpWage {
+    public static void EmpWageStatus(int num){
+        int Wage_HR = 20;
+        int Day_HRs = 8;
+        int Part_HRs = 4;
+/*
+ * Switch case
+ */
+        switch (num){
+            case 1:
+                System.out.println("Employee Present");
+                System.out.println("DayWage = " + Wage_HR * Day_HRs);
+                break;
+            case 2:
+                System.out.println("Employee Present");
+                System.out.println("DayWage with Part Time = " + Wage_HR *Part_HRs);
+                break;
+            case 0:
+                System.out.println("Employee Absent");
+                System.out.println("DayWage = "+0);
+        }
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
-		int Wage_HR = 20;
-		int Day_HRs = 8;
-		int Part_HRs = 4;
 
-		Random random = new Random();
-		int r = random.nextInt(3);
-		if (r == 1) {
-			// Employee Present or Absent and Assume Wage per hsr per day hrs part time and
-			// full time employee
-			System.out.println("Employee Present");
-			System.out.println("DayWage = " + Wage_HR * Day_HRs);
-		} else if (r == 2) {
-			System.out.println("Employee Present");
-			System.out.println("DayWage with Part Time = " + Wage_HR *Part_HRs); // part time Employee
-		}
+    }
 
-		else
-			System.out.println("Employee Absent");
-	}
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+        Random random = new Random();
+        int r = random.nextInt(3);
+        EmpWageStatus(r);
+
+    }
 }
