55494d9baf971da8a61ed41861f7ccb918bd45e2
Rachappa-Minajagi <m.rachoti142@gmail.com>
2022-02-24 14:57:18 +0530

    Displaying Wellcome Messages

---
+++ src/empwage_10/EmpWage.java
@@ -0,0 +1,10 @@
+package empwage_10;
+
+public class EmpWage {
+
+	public static void main(String[] args) {
+	System.out.println("WellCome to Employee Wage Computation Program");
+
+	}
+
+}
---
+++ src/empwage_10/package-info.java
@@ -0,0 +1,1 @@
+package empwage_10;
