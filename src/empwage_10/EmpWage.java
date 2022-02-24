58a0a112ab4de092440bf1a0f6fa294b33d6bbf9
RachappaMinajagi <59776797+RachappaMinajagi@users.noreply.github.com>
2022-02-24 15:30:28 +0530

    Employee Present or Absent

--- src/empwage_10/EmpWage.java
+++ src/empwage_10/EmpWage.java
@@ -2,9 +2,17 @@ package empwage_10;
 
 public class EmpWage {
 
+	import java.util.Random; // import Random class
+             
 	public static void main(String[] args) {
-	System.out.println("WellCome to Employee Wage Computation Program");
+		System.out.println("Welcome to Employee Wage Computation Program");
+		Random random = new Random();
+		int r = random.nextInt(2);
+		if (r == 1)
+			//Employee Present or Absent
+			System.out.println("Employee Present");
+		else
+			System.out.println("Employee Absent");
 
 	}
-
 }
